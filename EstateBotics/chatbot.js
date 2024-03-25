bb87f79b6a2369d348e530dfb8e69d19439ba024
LucaHemmerle <131297109+LucaHemmerle@users.noreply.github.com>
2024-03-25 21:23:50 +0100

    Add files via upload

---
+++ EstateBotics/chatbot.js
@@ -0,0 +1,24 @@
+(function(d, t) {
+  var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  v.onload = function() {
+    window.voiceflow.chat.load({
+      verify: { projectID: '656759ba18b33e0007c8ccff' },
+      url: 'https://general-runtime.voiceflow.com',
+      versionID: 'production',
+      assistant: {
+        stylesheet: 'https://www.estatebotics.de/falc.css'
+      }
+    });
+  };
+  v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+  v.type = "text/javascript";
+  s.parentNode.insertBefore(v, s);
+})(document, 'script');
+
+window.onload = function() {
+  if (window.innerWidth >= 1024) {
+    setTimeout(function() {
+      window.voiceflow.chat.open();
+    }, 1000);
+  }
+};
