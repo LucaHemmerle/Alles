2af8757a0d3a53b94c971872e02b4d3c143c19be
LucaHemmerle <131297109+LucaHemmerle@users.noreply.github.com>
2024-11-09 14:42:06 +0100

    Update chatbot.js

--- EstateBotics/chatbot.js
+++ EstateBotics/chatbot.js
@@ -6,7 +6,7 @@
       url: 'https://general-runtime.voiceflow.com',
       versionID: 'production',
       assistant: {
-        stylesheet: 'https://www.estatebotics.de/EstateBotics/Test.css'
+        stylesheet: 'https://www.estatebotics.de/EstateBotics/EstateBoticsNew.css'
       }
     });
   };
