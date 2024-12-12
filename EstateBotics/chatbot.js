41c1f345eb0d503006050517615f29129b912a76
LucaHemmerle <131297109+LucaHemmerle@users.noreply.github.com>
2024-12-12 11:59:47 +0100

    Update chatbot.js

--- EstateBotics/chatbot.js
+++ EstateBotics/chatbot.js
@@ -1,6 +1,8 @@
+<script>
 (function(d, t) {
   var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
   v.onload = function() {
+    // Chatbot laden
     window.voiceflow.chat.load({
       verify: { projectID: '671fa9b30a0ccb218be2f6fd' },
       url: 'https://general-runtime.voiceflow.com',
@@ -9,16 +11,18 @@
         stylesheet: 'https://www.estatebotics.de/EstateBotics/EstateBoticsNew.css'
       }
     });
+    
+    // Sobald die gesamte Seite fertig geladen ist
+    window.addEventListener('load', function() {
+      // Kurze Verzögerung, um sicherzustellen, dass alles initialisiert ist
+      setTimeout(function() {
+        window.voiceflow.chat.open();
+      }, 1000);
+    });
   };
+  
   v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
   v.type = "text/javascript";
   s.parentNode.insertBefore(v, s);
 })(document, 'script');
-
-window.onload = function() {
-  if (window.innerWidth >= 1024) {
-    setTimeout(function() {
-      window.voiceflow.chat.open();
-    }, 1000);
-  }
-};
+</script>
