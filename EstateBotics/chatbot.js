fbdbdbcf8adcc433f205faaf0520c28edb4b56ac
LucaHemmerle <131297109+LucaHemmerle@users.noreply.github.com>
2024-12-12 12:30:01 +0100

    Update chatbot.js

--- EstateBotics/chatbot.js
+++ EstateBotics/chatbot.js
@@ -9,21 +9,16 @@
         stylesheet: 'https://www.estatebotics.de/EstateBotics/EstateBoticsNew.css'
       }
     });
-
-    // Direkter Aufruf zum Öffnen des Chats nach dem Laden
-    window.voiceflow.chat.open();
   };
   v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
   v.type = "text/javascript";
   s.parentNode.insertBefore(v, s);
 })(document, 'script');
 
-// Entferne den alten window.onload und ersetze ihn
-document.addEventListener('DOMContentLoaded', function() {
-  // Stellen Sie sicher, dass der Chat geöffnet wird, unabhängig von der Bildschirmgröße
-  setTimeout(function() {
-    if (window.voiceflow && window.voiceflow.chat && window.voiceflow.chat.open) {
+window.onload = function() {
+  if (window.innerWidth >= 1024) {
+    setTimeout(function() {
       window.voiceflow.chat.open();
-    }
-  }, 1000);
-});
+    }, 1000);
+  }
+};
